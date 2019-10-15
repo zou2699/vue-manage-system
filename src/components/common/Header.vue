e3beaae50ce749af43adbb556bbeed172ab9891c
tux <tuxzou@gmail.com>
2019-10-15 15:06:20 +0800

    测试通过注销

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -41,7 +41,7 @@
                         <a href="https://github.com/lin-xin/vue-manage-system" target="_blank">
                             <el-dropdown-item>项目仓库</el-dropdown-item>
                         </a>
-                        <el-dropdown-item divided command="loginout">退出登录</el-dropdown-item>
+                        <el-dropdown-item divided command="logout">退出登录</el-dropdown-item>
                     </el-dropdown-menu>
                 </el-dropdown>
             </div>
@@ -68,8 +68,9 @@ export default {
     methods: {
         // 用户名下拉菜单选择事件
         handleCommand(command) {
-            if (command == 'loginout') {
+            if (command == 'logout') {
                 localStorage.removeItem('ms_username');
+                localStorage.removeItem('token');
                 this.$router.push('/login');
             }
         },
